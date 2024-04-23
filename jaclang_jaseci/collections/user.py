051473f96d6f6731e6440052043662d1b299e0ca
Yiping Kang <yipingkang93@gmail.com>
2024-04-23 10:29:26 -0400

    feat(collections): collections module for object management wrt mongodb

---
+++ jaclang_jaseci/collections/user.py
@@ -0,0 +1,25 @@
+"""UserCollection Interface."""
+
+from typing import Any, Mapping, Optional, TypeVar, Union
+
+from .base import BaseCollection
+
+T = TypeVar("T")
+
+
+class UserCollection(BaseCollection[T]):
+    """
+    User collection interface.
+
+    This interface is for User's Management.
+    You may override this if you wish to implement different structure
+    """
+
+    __collection__: Optional[str] = "user"
+    __excluded__: list[str] = ["password"]
+    __indexes__: list[dict] = [{"fields": ["email"], "unique": True}]
+
+    @classmethod
+    async def find_by_email(cls, email: str) -> Optional[Union[T, Mapping[str, Any]]]:
+        """Retrieve user via email."""
+        return await cls.find_one(filter={"email": email}, projection={})
